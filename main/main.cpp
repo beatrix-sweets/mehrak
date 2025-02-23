234947a6cdc515094032d8bf23f15e86a6910504
adolci <141414129+cipodrew@users.noreply.github.com>
2025-02-23 20:04:47 +0100

    feat: implementato animation cycle anche su ESP32
    introduzione del main loop per far funzionare l'animation cycle

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,7 @@
 #include "ESP32-HUB75-MatrixPanel-I2S-DMA.h"
 #include "esp_log.h"
 #include "freertos/task.h"
-#include "image.h"
+#include "images.h"
 #include <cstdint>
 
 MatrixPanel_I2S_DMA *dma_display = nullptr;
@@ -42,59 +42,65 @@ extern "C" void app_main() {
   ESP_LOGI(TAG, "Width: %d, Height: %d", dma_display->width(),
            dma_display->height());
 
-  // Clear the screen
-  dma_display->fillScreenRGB888(0, 0, 0);
+  /*========== MAIN LOOP =========*/
+  while (1) {
+    
+    // Clear the screen before lighting up with the current image
+    dma_display->fillScreenRGB888(0, 0, 0);
+    
+    ESP_LOGI(TAG, "Iterating through non black pixels...");
 
-  ESP_LOGI(TAG, "Iterating through non black pixels...");
+      Image_t currentImage = images[image_idx];
 
-  // Clear the screen before lighting up with the current image
-  dma_display->fillScreenRGB888(0, 0, 0);
+      // Print non-black pixels
+      for (int i = 0; i < currentImage.size; i++) {
+        uint16_t x = currentImage.data[i].x;
+        uint16_t y = currentImage.data[i].y;
+        if (x >= dma_display->width() || y >= dma_display->height()) {
+          ESP_LOGE(TAG, "pixel number %d  x=%d y=%d out of bound", i,
+                   currentImage.data[i].x, currentImage.data[i].y);
+          break; // unrecoverable error
+        }
+        dma_display->drawPixel(x, y,
+                               dma_display->color565(currentImage.data[i].r,
+                                                     currentImage.data[i].g,
+                                                     currentImage.data[i].b));
 
-    // Print non-black pixels
-    for (int i = 0; i < sizeof(image_data) / sizeof(image_data[0]); i++) {
-      uint16_t x = image_data[i].x;
-      uint16_t y = image_data[i].y;
-      if (x >= dma_display->width() || y >= dma_display->height()) {
-        ESP_LOGE(TAG, "pixel number %d  x=%d y=%d out of bound", i,
-                 image_data[i].x, image_data[i].y);
-        break; // unrecoverable error
+        if (isBooting) { // only display the boot animation for the first time
+          vTaskDelay(25 / portTICK_PERIOD_MS);
+        }
+
+        // NEED TO EXPLORE THIS API
+        /*  drawIcon draws a C style bitmap.
+        //  Example 10x5px bitmap of a yellow sun
+        //
+          int half_sun [50] = {
+              0x0000, 0x0000, 0x0000, 0xffe0, 0x0000, 0x0000, 0xffe0, 0x0000,
+        0x0000, 0x0000, 0x0000, 0xffe0, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+        0x0000, 0xffe0, 0x0000, 0x0000, 0x0000, 0x0000, 0xffe0, 0xffe0, 0xffe0,
+        0xffe0, 0x0000, 0x0000, 0x0000, 0xffe0, 0x0000, 0xffe0, 0xffe0, 0xffe0,
+        0xffe0, 0xffe0, 0xffe0, 0x0000, 0xffe0, 0x0000, 0x0000, 0xffe0, 0xffe0,
+        0xffe0, 0xffe0, 0xffe0, 0xffe0, 0x0000, 0x0000,
+          };
+
+          MatrixPanel_I2S_DMA matrix;
+
+          matrix.drawIcon (half_sun, 0,0,10,5);
+        */
+        // ESP_LOGI(TAG, "Lighting up line %d", y);
+
+        // Wait for a short duration to create a visible effect
+        /*vTaskDelay(100 / portTICK_PERIOD_MS);*/
       }
-      dma_display->drawPixel(x, y,
-                             dma_display->color565(image_data[i].r,
-                                                   image_data[i].g,
-                                                   image_data[i].b));
 
+      ESP_LOGI(TAG, "Finished iterating through all lines.");
       if (isBooting) { // only display the boot animation for the first time
-        vTaskDelay(25 / portTICK_PERIOD_MS);
+      isBooting = false;
       }
+      image_idx = (image_idx + 1) % num_images;
 
-      // NEED TO EXPLORE THIS API
-      /*  drawIcon draws a C style bitmap.
-      //  Example 10x5px bitmap of a yellow sun
-      //
-        int half_sun [50] = {
-            0x0000, 0x0000, 0x0000, 0xffe0, 0x0000, 0x0000, 0xffe0, 0x0000,
-      0x0000, 0x0000, 0x0000, 0xffe0, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-      0x0000, 0xffe0, 0x0000, 0x0000, 0x0000, 0x0000, 0xffe0, 0xffe0, 0xffe0,
-      0xffe0, 0x0000, 0x0000, 0x0000, 0xffe0, 0x0000, 0xffe0, 0xffe0, 0xffe0,
-      0xffe0, 0xffe0, 0xffe0, 0x0000, 0xffe0, 0x0000, 0x0000, 0xffe0, 0xffe0,
-      0xffe0, 0xffe0, 0xffe0, 0xffe0, 0x0000, 0x0000,
-        };
-
-        MatrixPanel_I2S_DMA matrix;
-
-        matrix.drawIcon (half_sun, 0,0,10,5);
-      */
-      // ESP_LOGI(TAG, "Lighting up line %d", y);
-
-      // Wait for a short duration to create a visible effect
-      /*vTaskDelay(100 / portTICK_PERIOD_MS);*/
-    }
-
-    ESP_LOGI(TAG, "Finished iterating through all lines.");
-    isBooting = false;
-
-    /*vTaskDelay(2000 / portTICK_PERIOD_MS);*/
-    vTaskDelay(20000 / portTICK_PERIOD_MS); // set a long period of idle, after
+      /*vTaskDelay(2000 / portTICK_PERIOD_MS);*/
+      vTaskDelay(2000 / portTICK_PERIOD_MS); // set a long period of idle, after
                                             // which we can skip to image2?
+  }
 }
